372afdf0f34128dd6b8223f20bc13242265acdc0
Xuwenhao <1364041941@qq.com>
2016-08-07 12:25:16 +0800

    Today is Aug 7 2016,My Name is XuWenHao

--- GitTest_xu_87/GitTest_xu_87/Program.cs
+++ GitTest_xu_87/GitTest_xu_87/Program.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 
 namespace GitTest_xu_87
 {
+    public delegate void MyDel(string a);
+    class Person {
+       public static void seng(string a) {
+           Console.WriteLine("我的名字叫"+a);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
+          // MyDel my=new MyDel(Person.seng);
+            MyDel my= Person.seng;
+            my("令狐冲");
+           Console.ReadKey();
+           
         }
     }
 }
